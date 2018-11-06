fa027e715a4aaac47e920c710a3a8b630792dbaf
wangss <1063889643@qq.com>
2018-11-06 11:16:07 +0800

    + New Design Mode Learn

---
+++ src/com/arthur/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package com.arthur.singleton;
+
+/**
+ * 饿汉式单例
+ */
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
---
+++ src/com/arthur/singleton/Singleton2.java
@@ -0,0 +1,12 @@
+package com.arthur.singleton;
+
+/**
+ * 懒汉式单例
+ */
+public class Singleton2 {
+    private static Singleton2 singleton2;
+
+    private Singleton2() {
+
+    }
+}
